e1a34b25c0053fb00795d4d2e8ded70a8efc5ed5
Sönke Hahn <soenkehahn@gmail.com>
2019-10-13 14:41:16 -0400

    add pager support

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+use pager::Pager;
 use std::fs;
 use std::io::Write;
 use std::path::PathBuf;
 
 type R<A> = Result<A, Box<dyn std::error::Error>>;
 
-fn wrap_main(action: fn(args: &mut dyn Iterator<Item = String>, stderr: &mut dyn Write) -> R<()>) {
-    let mut stderr = std::io::stderr();
-    let exitcode = match action(&mut std::env::args(), &mut stderr) {
+fn wrap_main(action: fn(args: &mut dyn Iterator<Item = String>, stdout: &mut dyn Write) -> R<()>) {
+    Pager::with_pager("less -RFX").setup();
+    let mut stdout = std::io::stdout();
+    let exitcode = match action(&mut std::env::args(), &mut stdout) {
         Ok(()) => 0,
         Err(error) => {
-            stderr.write_all(format!("{}", error).as_bytes()).unwrap();
+            stdout.write_all(format!("{}", error).as_bytes()).unwrap();
             1
         }
     };
@@ -20,13 +22,13 @@ fn main() {
     wrap_main(run);
 }
 
-fn run(args: &mut dyn Iterator<Item = String>, stderr: &mut dyn Write) -> R<()> {
+fn run(args: &mut dyn Iterator<Item = String>, stdout: &mut dyn Write) -> R<()> {
     let file = PathBuf::from(args.nth(1).unwrap());
     if !file.exists() {
         return Err(format!("path not found: {}\n", file.to_string_lossy()).into());
     }
     let contents = fs::read(file)?;
-    stderr.write_all(&contents)?;
+    stdout.write_all(&contents)?;
     Ok(())
 }
 
@@ -40,25 +42,25 @@ mod test {
     fn cats_files() -> R<()> {
         let tempdir = TempDir::new("si-test")?;
         fs::write(tempdir.path().join("foo"), "bar")?;
-        let mut stderr = Cursor::new(vec![]);
+        let mut stdout = Cursor::new(vec![]);
         run(
             &mut vec![
                 "si".to_string(),
                 tempdir.path().join("foo").to_string_lossy().into_owned(),
             ]
             .into_iter(),
-            &mut stderr,
+            &mut stdout,
         )?;
-        assert_eq!(String::from_utf8_lossy(stderr.get_ref()), "bar");
+        assert_eq!(String::from_utf8_lossy(stdout.get_ref()), "bar");
         Ok(())
     }
 
     #[test]
     fn path_not_found() -> R<()> {
-        let mut stderr = Cursor::new(vec![]);
+        let mut stdout = Cursor::new(vec![]);
         let result = run(
             &mut vec!["si".to_string(), "does_not_exist.txt".to_string()].into_iter(),
-            &mut stderr,
+            &mut stdout,
         );
         assert_eq!(
             result.map_err(|x| x.to_string()),
