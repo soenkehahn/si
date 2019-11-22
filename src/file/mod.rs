46b8921b882bbd3a1e6224a2a3c41f54309d37e7
Sönke Hahn <soenkehahn@gmail.com>
2019-11-22 00:53:32 -0500

    implement line numbers

--- src/file/mod.rs
+++ src/file/mod.rs
@@ -11,12 +11,35 @@ pub fn output(stdout: &mut dyn Write, file: PathBuf) -> R<()> {
     let size = fs::metadata(&file)?.len();
     writeln!(stdout, "file: {}, {} bytes", file.to_string_lossy(), size)?;
     write_separator(stdout)?;
-    for chunk in colorize(Stream::read_utf8_file(&file)?)
-        .flat_map(|x| Stream::from_iterator(x.chars().collect::<Vec<_>>().into_iter()))
-    {
+    for chunk in add_line_numbers(
+        colorize(Stream::read_utf8_file(&file)?)
+            .flat_map(|x| Stream::from_iterator(x.chars().collect::<Vec<_>>().into_iter())),
+    ) {
         write!(stdout, "{}", chunk)?;
     }
     Ok(())
+}
+
+fn add_line_numbers(mut input: Stream<char>) -> Stream<String> {
+    let mut line_start = true;
+    let mut line_number = 0;
+    Stream::new(move || match input.next() {
+        Some(char) if line_start => {
+            line_number += 1;
+            Some(if char == '\n' {
+                format!("{} |\n", line_number)
+            } else {
+                line_start = false;
+                format!("{} | {}", line_number, char)
+            })
+        }
+        Some('\n') => {
+            line_start = true;
+            Some("\n".to_string())
+        }
+        Some(char) => Some(char.to_string()),
+        None => None,
+    })
 }
 
 #[cfg(test)]
@@ -45,5 +68,27 @@ mod test {
             "---".yellow().bold().to_string()
         );
         Ok(())
+    }
+
+    mod line_numbers {
+        use super::*;
+
+        #[test]
+        fn shows_line_numbers() -> R<()> {
+            let mut setup = setup()?;
+            fs::write(setup.tempdir().join("foo"), "foo\nbar\nbaz\n")?;
+            setup.run(vec!["foo".to_string()])?;
+            assert_eq!(drop_stats(setup.stdout()), "1 | foo\n2 | bar\n3 | baz\n");
+            Ok(())
+        }
+
+        #[test]
+        fn works_for_consecutive_newlines() -> R<()> {
+            let mut setup = setup()?;
+            fs::write(setup.tempdir().join("foo"), "foo\n\nbar\n")?;
+            setup.run(vec!["foo".to_string()])?;
+            assert_eq!(drop_stats(setup.stdout()), "1 | foo\n2 |\n3 | bar\n");
+            Ok(())
+        }
     }
 }
